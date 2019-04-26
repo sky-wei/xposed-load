d73b7e02d1a2086525998974600b2190eadf27f1
sky <jingcai.wei@163.com>
2019-04-26 14:38:29 +0800

    适配root命令路径

--- app/src/main/java/com/sky/xposed/load/util/SystemUtil.kt
+++ app/src/main/java/com/sky/xposed/load/util/SystemUtil.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.text.TextUtils
 import com.sky.android.common.util.FileUtil
 import java.io.BufferedReader
+import java.io.File
 import java.io.InputStreamReader
 
 /**
@@ -13,6 +14,23 @@ import java.io.InputStreamReader
 object SystemUtil {
 
     val TAG = "SystemUtil"
+
+    private val CALLER = "/data/system/caller"
+    private val CALLER81 = "/debuging/caller"
+    private val DEFAULT = "su"
+
+    /**
+     * 获取Root路径
+     * @return
+     */
+    fun getRootPath(): String {
+        if (File(CALLER).exists()) {
+            return CALLER
+        } else if (File(CALLER81).exists()) {
+            return CALLER81
+        }
+        return DEFAULT
+    }
 
     fun killApp(context: Context, packageName: String, root: Boolean) {
 
@@ -40,20 +58,14 @@ object SystemUtil {
         return false
     }
 
+    /**
+     * 强制停止指定的包
+     */
     fun forceStop(packageName: String) {
 
         if (TextUtils.isEmpty(packageName)) return
 
-        val cmd = "/data/system/caller -c am force-stop $packageName"
-
-        Alog.d(TAG, "CMD: $cmd")
-
-        try {
-            val runtime = Runtime.getRuntime()
-            runtime.exec(cmd)
-        } catch (e: Throwable) {
-            Alog.e(TAG, "执行命令异常", e)
-        }
+        execRoot("am force-stop $packageName")
     }
 
     /**
@@ -66,7 +78,7 @@ object SystemUtil {
             throw NullPointerException("执行任务不能为空")
         }
 
-        return exec("su -c $cmd")
+        return exec("${getRootPath()} -c $cmd")
     }
 
     /**
